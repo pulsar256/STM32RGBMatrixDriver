5b69ad9a30a125a8524e1b109fcae5a3a831e742
Paul Rogalinski <paul@paul.vc>
2015-01-20 17:08:36 +0100

    - migrating to stm32f103

--- src/main.c
+++ src/main.c
@@ -1,100 +1,56 @@
-/**
- *
- * Bit-Banging of 32*32 RGB Matrix Panels using a STM32F401
- *
- * Author: Paul Rogalinski, paul@paul.vc
- * License: Public Domain, see LICENSE.txt and http://unlicense.org/
- *
- * This is for educational purposes only, driving such panels with
- * bitbanging is inefficient and pretty much a bad idea. One might
- * this code useful for prototyping purposes related to
- * the 32*32 or 32*16 RGB Panels before moving on to a more suited
- * hardware such as FPGA or at least some DMA Kung-Fu.
- *
- *
- * Pin Assignment
- * ==============================================
- *
- * PORT A: Color Data
- * ----------------------------------------------
- *
- * r1   PORT A: GPIO_PIN_5
- * g1   PORT A: GPIO_PIN_6
- * b1   PORT A: GPIO_PIN_7
- *
- * r2   PORT A: GPIO_PIN_8
- * g2   PORT A: GPIO_PIN_9
- * b2   PORT A: GPIO_PIN_10
- *
- *
- * Address Select, Output Enable
- * ----------------------------------------------
- *
- * A    PORT B: GPIO_PIN_5
- * B    PORT B: GPIO_PIN_6
- * C    PORT B: GPIO_PIN_8
- * D    PORT B: GPIO_PIN_9
- * OE   PORT B: GPIO_PIN_10
- *
- * USART
- * ----------------------------------------------
- * TX		PORT A: GPIO_PIN_2
- * RX		PORT A: GPIO_PIN_3
- *
- * Clock & Latch
- * ----------------------------------------------
- * CLK  PORT C: GPIO_PIN_7
- * STB  PORT C: GPIO_PIN_0
- */
+#include <stdio.h>
+#include "stm32f10x.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_rcc.h"
+#include "math.h"
+#include "string.h"
+#include "stdlib.h"
 
 #include "main.h"
-#include "stm32f4xx_it.h"
+#include "matrix_config.h"
 
-UART_HandleTypeDef uartHandle;
-volatile ITStatus uartReady = SET;
-
-const int waits[] = {10,20,40,80,160,320,640,1280};
-const int scan =  MATRIX_HEIGHT/2;
+const int waits[] = { 5, 10, 20, 40, 80, 160, 320, 640 };
+const int scan = MATRIX_HEIGHT / 2;
 uint8_t gammaTable[256];
 uint32_t framebuffer[MATRIX_SIZE];
 char* uartAliveMsg = "200 frames passed";
 
-int	main() {
+// ----- Timing definitions -------------------------------------------------
 
-	initGPIO();
-	initUART(&uartHandle);
+int main() {
+	setupRGBMatrixPorts();
 
-	DISP_OFF;
+	LED_PORT->BRR = LED_P;
 
 	// precalculate the gamma lookup table
-	for (int i=0; i<256; i++) gammaTable[i] = 255*pow((i/256.0),1.6);
+	for (int i = 0; i < 256; i++) gammaTable[i] = 255 * pow((i / 256.0), 1.8);
 
 	// clear framebuffer
-	memset (framebuffer, 0, sizeof(framebuffer));
+	memset(framebuffer, 0, sizeof(framebuffer));
+
 
 	// test pattern, light up a led in each corner
-	framebuffer[0] =    0x00000050;
-	framebuffer[31] =   0x00005000;
-	framebuffer[992] =  0x00500000;
+	framebuffer[0]    = 0x00000050;
+	framebuffer[31]   = 0x00005000;
+	framebuffer[992]  = 0x00500000;
 	framebuffer[1023] = 0x00505000;
 
 	// display test pattern for 500 frames
-	for (int i=0; i<500; i++){
+	for (int i = 0; i < 500; i++) {
 		displayBuffer(framebuffer);
 	}
 
+	LED_PORT->BSRR = LED_P;
+
 	int frame = 0;
-	while(1){
+	while (1) {
 		displayBuffer(framebuffer);
-		if (++frame % 5 == 0) randomizeFramebuffer(framebuffer);
+		if (++frame % 5 == 0)	randomizeFramebuffer(framebuffer);
 		if (frame % 200 == 0) {
-			while (uartReady != SET){}
-			uartReady = RESET;
-			if(HAL_UART_Transmit_DMA(&uartHandle, (uint8_t*) uartAliveMsg, 17)!= HAL_OK) while(1);
+			LED_PORT->ODR ^= LED_P;
 		}
 	}
 }
-
 
 /**
  * Displays the buffer on the display using binary encoding (PWM equivalent).
@@ -137,47 +93,46 @@ void randomizeFramebuffer(uint32_t buffer[]) {
  * sets the row on the row gpio ports
  */
 void setRow(int row) {
-
 	// todo: perhaps a lookup table could give us a tiny boost here.
 
-	if (row & 0b0001) GPIOB->BSRRL = GPIO_PIN_5;
-	else GPIOB->BSRRH = GPIO_PIN_5;
+	if (row & 0b0001) MTX_PORT->BSRR = MTX_PA;
+	else MTX_PORT->BRR = MTX_PA;
 
-	if (row & 0b0010) GPIOB->BSRRL = GPIO_PIN_6;
-	else GPIOB->BSRRH = GPIO_PIN_6;
+	if (row & 0b0010) MTX_PORT->BSRR = MTX_PB;
+	else MTX_PORT->BRR = MTX_PB;
 
-	if (row & 0b0100) GPIOB->BSRRL = GPIO_PIN_8;
-	else GPIOB->BSRRH = GPIO_PIN_8;
+	if (row & 0b0100) MTX_PORT->BSRR = MTX_PC;
+	else MTX_PORT->BRR = MTX_PC;
 
-	if (row & 0b1000) GPIOB->BSRRL = GPIO_PIN_9;
-	else GPIOB->BSRRH = GPIO_PIN_9;
+	if (row & 0b1000) MTX_PORT->BSRR = MTX_PD;
+	else MTX_PORT->BRR = MTX_PD;
 }
 
 
 /**
  * loads rgb1 and rgb2 gpio ports with the given bitplane
  */
-void setRGB(uint32_t rgb1, uint32_t rgb2, uint8_t plane ){
+void setRGB(uint32_t rgb1, uint32_t rgb2, uint8_t plane) {
 	// using bitshifting seems to be faster due to gcc optimization
 	// than using a bitmask lookup table here.
 
-	if (rgb1 & (1 << plane))        GPIOA->BSRRL = GPIO_PIN_5;
-	else                            GPIOA->BSRRH = GPIO_PIN_5;
+	if (rgb1 & (1 << plane))        MTX_PORT->BSRR = MTX_PR0;
+	else                            MTX_PORT->BRR  = MTX_PR0;
 
-	if (rgb1 & (1 << (plane + 8))) 	GPIOA->BSRRL = GPIO_PIN_6;
-	else                            GPIOA->BSRRH = GPIO_PIN_6;
+	if (rgb1 & (1 << (plane + 8))) 	MTX_PORT->BSRR = MTX_PG0;
+	else                            MTX_PORT->BRR  = MTX_PG0;
 
-	if (rgb1 & (1 << (plane + 16))) GPIOA->BSRRL = GPIO_PIN_7;
-	else                            GPIOA->BSRRH = GPIO_PIN_7;
+	if (rgb1 & (1 << (plane + 16))) MTX_PORT->BSRR = MTX_PB0;
+	else                            MTX_PORT->BRR  = MTX_PB0;
 
-	if (rgb2 & (1 << plane))        GPIOA->BSRRL = GPIO_PIN_8;
-	else                            GPIOA->BSRRH = GPIO_PIN_8;
+	if (rgb2 & (1 << plane))        MTX_PORT->BSRR = MTX_PR1;
+	else                            MTX_PORT->BRR  = MTX_PR1;
 
-	if (rgb2 & (1 << (plane + 8))) 	GPIOA->BSRRL = GPIO_PIN_9;
-	else                            GPIOA->BSRRH = GPIO_PIN_9;
+	if (rgb2 & (1 << (plane + 8))) 	MTX_PORT->BSRR = MTX_PG1;
+	else                            MTX_PORT->BRR  = MTX_PG1;
 
-	if (rgb2 & (1 << (plane + 16))) GPIOA->BSRRL = GPIO_PIN_10;
-	else                            GPIOA->BSRRH = GPIO_PIN_10;
+	if (rgb2 & (1 << (plane + 16))) MTX_PORT->BSRR = MTX_PB1;
+	else                            MTX_PORT->BRR  = MTX_PB1;
 }
 
 
@@ -192,4 +147,31 @@ void showLine(int amount) {
 }
 
 
+void setupRGBMatrixPorts() {
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	RCC_APB2PeriphClockCmd(MTX_RCCPB, ENABLE);                        // enable matrix GPIO port
+	RCC_APB2PeriphClockCmd(LED_RCCPB, ENABLE);                        // enable LED GPIO port
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);              // enable AFIO module, needed to release (remap the AlternateFunction) the JTAG Pins
+
+	GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE); 					// disable JTAG Debugging, we only need SWD. Releases PB3 and PB4
+
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+
+	GPIO_InitStructure.GPIO_Pin = MTX_PR0 | MTX_PG0 | MTX_PB0;        // RGB0
+	GPIO_Init(MTX_PORT, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin = MTX_PR1 | MTX_PG1 | MTX_PB1;        // RGB1
+	GPIO_Init(MTX_PORT, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin = MTX_PA | MTX_PB | MTX_PC | MTX_PD;  // ABCD
+	GPIO_Init(MTX_PORT, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin = MTX_PSTB | MTX_POE | MTX_PCLK;      // Strobe, OutputEnable, Clock
+	GPIO_Init(MTX_PORT, &GPIO_InitStructure);
+
+	GPIO_InitStructure.GPIO_Pin = LED_P;                              // Status LED
+	GPIO_Init(LED_PORT, &GPIO_InitStructure);
+}
 
